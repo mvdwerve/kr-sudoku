d267ab1b61d7f8a9e8081a055bb9d7c2c241c43f
Michael van der Werve <mvdwerve@outlook.com>
2017-09-19 14:39:55 +0200

    it s wrong but it's an improvement

--- sudoku.py
+++ sudoku.py
@@ -15,7 +15,7 @@ class Sudoku:
 
         # generate _all_ the variables, in an n*n*n matrix, because each variable has n options in n*n places
         # the indices are row, column, variables, or rather self.vars_[row][column][var]
-        self.names_ = ['%d_%d_%d' % (i, j, k + 1) for i, j, k in product(range(self.n_), repeat=3)]
+        self.names_ = ['%d_%d__%d' % (i, j, k + 1) for i, j, k in product(range(self.n_), repeat=3)]
         #self.syms_ = dict(zip(self.names_, sympy.symbols(self.names_)))
 
         # the context and the found highest
@@ -23,7 +23,7 @@ class Sudoku:
         self.highest_ = 0
 
         # nothing given yet
-        self.given_ = None
+        self.total_ = []
 
         # add the givens
         for i, c in enumerate(given):
@@ -36,62 +36,53 @@ class Sudoku:
             row = int((i - col) / 9) 
             
             # get the variable and add it as a unit clause
-            var = self.var(row, col, int(c) - 1)
-            self.given_ = var if self.given_ == None else self.given_ & var
+            self.add_clause([self.var(row, col, int(c) - 1)])
 
+    def add_clause(self, clause):
+        # todo: don't add double clauses
+        self.total_.append(clause)
 
     def rules(self):
-        if self.rules_ != None:
-            return self.rules_
+        # 1. every square _must_ have a single one set
+        for row, column in product(range(self.n_), repeat=2):
+            self.add_clause([self.var(row, column, v) for v in range(self.n_)])
 
-        # we are going to generate the rules
+        # 2. every number must be set in a row at least once
+        for v in range(self.n_):
+            for i in range(self.n_):
+                self.add_clause([self.var(i, j, v) for j in range(self.n_)])
+                self.add_clause([self.var(j, i, v) for j in range(self.n_)])
 
-        # 1. every square may only have a _single_ variable set
-        # variable ^ all other variables in the depth
-        one = reduce(operator.and_, [reduce(operator.xor, [self.var(i, j, k) for k in range(self.n_)]) for i, j in product(range(self.n_), repeat=2)])
+        # blocks should have every number _at least_ once
+        for v in range(self.n_):
+            for br, bc in product(range(self.square_), repeat=2):
+                self.add_clause([self.var(br + i, bc + j, v) for i, j in product(range(self.square_), repeat=2)])
 
-        # 2. every row and column _must_ have all numbers set differently
-        # variables in row ^ all other of the same variable in row
-        # variables in column ^ all other of the same variable in column
-        two_row = reduce(operator.and_, [reduce(operator.xor, [self.var(k, j, i) for k in range(self.n_)]) for i, j in product(range(self.n_), repeat=2)])
-        two_col = reduce(operator.and_, [reduce(operator.xor, [self.var(j, k, i) for k in range(self.n_)]) for i, j in product(range(self.n_), repeat=2)])
-        
-        # 3. all elements in a block must be set precisely once
-        # for every variable in block ^ other variables in the block
-        block_rule = reduce(operator.and_, [reduce(operator.xor, [self.var(br + i, bc + j, k) for i, j in product(range(self.square_), repeat=2)]) for br, bc, k in product(range(self.square_), range(self.square_), range(self.n_))])
-
-        # all rules should be true
-        total = one & two_row & two_col & block_rule & self.given_
+        # each cell may have _at most_ a single entry
+        for r, c in product(range(self.n_), repeat=2):
+            for v in range(self.n_ - 1):
+                self.add_clause(["-" + self.var(r, c, v), "-" + self.var(r, c, v + 1)])
 
         # finally, all the rules should be true, and in cnf
-        return total.simplify().tseytin(self.ctx_)
+        return self.total_
 
     def var(self, row, column, value):
-        return self.ctx_.get_var('%d_%d_%d' % (row, column, value + 1))
+        return '%d_%d__%d' % (row, column, value + 1)
 
     def extract(self, var):
         name = str(var)
-        return name[1:] if name[0] == '~' else name 
+        return name[1:] if name[0] == '-' else name 
 
     def index(self, var):
-        name = self.extract(var)
-        if 'a' in name:
-            found = int(name[2:])
-            self.highest_ = max(self.highest_, found)
-            return len(self.names_) + found
-        return self.names_.index(name) + 1
+        mult = -1 if var[0] == '-' else 1
+        return mult * (self.names_.index(self.extract(var)) + 1)
 
     def dimac_sentence(self, expr):
-        if isinstance(expr, bx.Variable):
-            return str(self.index(expr))
-        elif isinstance(expr, bx.Complement):
-            return "-" + str(self.index(expr))
-        else:
-            return " ".join([self.dimac_sentence(ex) for ex in expr.args])
+        return " ".join([str(self.index(ex)) for ex in expr])
 
     def dimacs(self):
         # get the rules once
-        rules = self.rules().args
+        rules = self.rules()
 
         # do a nice list comprehension
         l = "\n".join([self.dimac_sentence(rule) + " 0" for rule in rules])
@@ -102,6 +93,26 @@ class Sudoku:
         # simply append
         return header + l
 
+    def solve(self):
+        # get the rules
+        rules = self.rules()
+
+        # now we map it to the numeric vars
+        cnf = [[self.index(ex) for ex in rule] for rule in rules]
+
+        # solve using pycosat
+        return pycosat.solve(cnf)
+
+    def print(self, solved=True):
+        # first, get all the givens  
+        filled = [self.index(el[0]) for el in self.rules() if len(el) == 1] if not solved else [el for el in self.solve() if el > 0]
+        
+        # and we get the indices that
+        print(len(filled))
+
+        # now we convert these numbers to 
+
+
 if __name__=="__main__":
     # load the 100 sudokus
     sudokus = pd.read_csv('sudoku.csv')
@@ -109,5 +120,8 @@ if __name__=="__main__":
     # create a normal standard sudoku
     sudoku = Sudoku(int(argv[1]), sudokus.iloc[0][0])
 
+    sudoku.print(solved=False)
+    sudoku.print(solved=True)
+
     # and output the rules
-    print(sudoku.dimacs(), end="")
+    #print(sudoku.dimacs(), end="")
