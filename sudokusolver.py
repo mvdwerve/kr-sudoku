efe418bf0a63af94a9fd09ed13e072666c2d09e3
Michael van der Werve <mvdwerve@outlook.com>
2017-09-19 11:08:52 +0200

    WIP

--- sudokusolver.py
+++ sudokusolver.py
@@ -1,10 +1,14 @@
 
 from solver import parse, Solver
 from sys import argv
+import numpy as np
+import pandas as pd
 
 if __name__=="__main__":
     # open the rules
-    solver = parse(open(argv[1], 'r').read())
+    solver = parse(open('sudoku_rules_9x9.cnf', 'r').read())
+    sudokus = pd.read_csv('sudoku.csv')
+    print(sudokus[1, :])
 
     #string = arguments.file.read()
     #solver = parse(string)
