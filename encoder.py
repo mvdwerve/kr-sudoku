d52a39baf6542ef37f22b1884b81949115a61096
Michael van der Werve <mvdwerve@outlook.com>
2017-10-02 17:25:56 +0200

    also efficient rules

--- encoder.py
+++ encoder.py
@@ -74,14 +74,14 @@ class NaiveEncoder(Encoder):
         # http://sat.inesc.pt/~ines/publications/aimath06.pdf
 
         # Minimal encoding: 81 nine-ary encodings, 8748 binary
-        # 1. at least one number in each entry
+        # 1. at least one number in each entry (Cell defined)
         self.add_clauses([[self.mapper.var(x, y, z) for z in range(self.mapper.n)] for x, y in product(range(self.mapper.n), repeat=2)])
 
-        # 2. every number appears at most once in each row/column
+        # 2. every number appears at most once in each row/column (Row/Col defined)
         self.add_clauses([["-" + self.mapper.var(x, y, z), "-" + self.mapper.var(i, y, z)] for y, z, x in product(range(self.mapper.n), range(self.mapper.n), range(self.mapper.n - 1)) for i in range(x + 1, self.mapper.n)])
         self.add_clauses([["-" + self.mapper.var(x, y, z), "-" + self.mapper.var(x, i, z)] for x, z, y in product(range(self.mapper.n), range(self.mapper.n), range(self.mapper.n - 1)) for i in range(y + 1, self.mapper.n)])
 
-        # 3. each number appears at most once in each 3x3 subgrid
+        # 3. each number appears at most once in each 3x3 subgrid (Block unique)
         self.add_clauses([["-" + self.mapper.var(self.mapper.square*i + x, self.mapper.square*j + y, z), "-" + self.mapper.var(self.mapper.square*i + x, self.mapper.square*j + k, z)] for z, (i, j, x, y) in product(range(self.mapper.n), product(range(self.mapper.square), repeat=4)) for k in range(y + 1, self.mapper.square)])
         self.add_clauses([["-" + self.mapper.var(self.mapper.square*i + x, self.mapper.square*j + y, z), "-" + self.mapper.var(self.mapper.square*i + k, self.mapper.square*j + l, z)] for z, (i, j, l, x, y) in product(range(self.mapper.n), product(range(self.mapper.square), repeat=5)) for k in range(x + 1, self.mapper.square)])
 
@@ -92,39 +92,56 @@ class ExtendedNaiveEncoder(NaiveEncoder):
         # first we make the naive encoding
         NaiveEncoder.encode_rules(self)
 
-        # 1. at most one number in each entry
+        # 1. at most one number in each entry (cell unique)
         self.add_clauses([["-" + self.mapper.var(x, y, z), "-" + self.mapper.var(x, y, i)] for x, y, z in product(range(self.mapper.n), range(self.mapper.n), range(self.mapper.n - 1)) for i in range(z + 1, self.mapper.n)])
 
-        # 2. every number appears at least once in each row/column
+        # 2. every number appears at least once in each row/column (row/col unique)
         self.add_clauses([[self.mapper.var(x, y, z) for x in range(self.mapper.n)] for y, z in product(range(self.mapper.n), repeat=2)])
         self.add_clauses([[self.mapper.var(x, y, z) for y in range(self.mapper.n)] for x, z in product(range(self.mapper.n), repeat=2)])
 
-        # 3. each number appears at least once in each 3x3 subgrid
+        # 3. each number appears at least once in each 3x3 subgrid (block defined)
         self.add_clauses([[self.mapper.var(self.mapper.square*i + x, self.mapper.square*j + y, z) for z in range(self.mapper.n)] for i, j, x, y in product(range(self.mapper.square), repeat=4)])
 
 class EfficientEncoder(Encoder):
     def encode_rules(self):
-        pass
+        # [Weber, 2005] thought of this encoding, it's in-between
+        #: cell defined, cell unique, row unique, column unique, block unique
+
+        # 1. at least one number in each entry (Cell defined)
+        self.add_clauses([[self.mapper.var(x, y, z) for z in range(self.mapper.n)] for x, y in product(range(self.mapper.n), repeat=2)])
+        
+        # 2. every number appears at least once in each row/column (row/col unique)
+        self.add_clauses([[self.mapper.var(x, y, z) for x in range(self.mapper.n)] for y, z in product(range(self.mapper.n), repeat=2)])
+        self.add_clauses([[self.mapper.var(x, y, z) for y in range(self.mapper.n)] for x, z in product(range(self.mapper.n), repeat=2)])
+
+        # 3. each number appears at most once in each 3x3 subgrid (Block unique)
+        self.add_clauses([["-" + self.mapper.var(self.mapper.square*i + x, self.mapper.square*j + y, z), "-" + self.mapper.var(self.mapper.square*i + x, self.mapper.square*j + k, z)] for z, (i, j, x, y) in product(range(self.mapper.n), product(range(self.mapper.square), repeat=4)) for k in range(y + 1, self.mapper.square)])
+        self.add_clauses([["-" + self.mapper.var(self.mapper.square*i + x, self.mapper.square*j + y, z), "-" + self.mapper.var(self.mapper.square*i + k, self.mapper.square*j + l, z)] for z, (i, j, l, x, y) in product(range(self.mapper.n), product(range(self.mapper.square), repeat=5)) for k in range(x + 1, self.mapper.square)])
+
 
 if __name__=="__main__":
     # we need the argument parser
     import argparse
 
+    # make the encoders
+    encoders = {
+        'naive': NaiveEncoder,
+        'extended': ExtendedNaiveEncoder,
+        'efficient': EfficientEncoder
+    }
+
     # parse arguments
     parser = argparse.ArgumentParser(description='Encode the rules for an NxN sudoku.')
     parser.add_argument('--square', type=int, default=3, help='Square to use, e.g. 2 is for a 4x4 sudoku and 3 is for a 3^2 = 9x9 sudoku, etc.')
-    parser.add_argument('--encoder', type=str, default='Naive', choices=['Naive', 'ExtendedNaive'])
+    parser.add_argument('--encoder', type=str, default='naive', choices=list(encoders.keys()))
     args = parser.parse_args()
 
     # get the square for the mapper and startup the dimacs
     mapper = SudokuMapper(args.square)
     dimacs = DimacsMapper(mapper)
 
-    # we check the encoder
-    if args.encoder == 'Naive':
-        encoder = NaiveEncoder(mapper)
-    elif args.encoder == 'ExtendedNaive':
-        encoder = ExtendedNaiveEncoder(mapper)
+    # get the encoder
+    encoder = encoders[args.encoder](mapper)
     
     # now we actually encode the rules
     print(dimacs.encode(encoder.encode()))
